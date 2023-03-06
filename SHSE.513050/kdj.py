ef88332d7705daba83f8baa79ebd800636a3abae
mura <1807709057@qq.com>
2023-03-07 01:35:31 +0800

    test kdj

--- SHSE.513050/kdj.py
+++ SHSE.513050/kdj.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 # data = pd.DataFrame({
 #     'high': [12.0, 13.0, 14.0, 15.0, 16.0],
@@ -7,6 +6,66 @@ import numpy as np
 #     'close': [11.0, 12.0, 13.0, 14.0, 15.0],
 # })
 
+
+def KDJ(high, low, close, n=9, m1=3, m2=3):
+    # 最近n天的最高价（HHV)、最低价（LLV）以及当日收盘价(close)
+    hhv = high.rolling(n, min_periods=0).max()
+    llv = low.rolling(n, min_periods=0).min()
+    rsv = (close - llv) / (hhv - llv) * 100
+    k = rsv.ewm(alpha=1/m1,ignore_na=True,min_periods=0,adjust=False).mean()    
+    d = k.ewm(alpha=1/m2,ignore_na=True,min_periods=0,adjust=False).mean() 
+    j = 3 * k - 2 * d
+    return k, d, j
+
+
+def calc_kdj(data_list,n=9, m1=3, m2=3):
+    high_list = []
+    low_list = []
+    rsv_list = []
+    k_list = []
+    d_list = []
+    j_list = []
+
+    for bar in data_list:
+        high, low, close = bar['high'], bar['low'], bar['close']
+        print(high, low, close)
+        high_list.append(high)
+        low_list.append(low)
+
+        if len(high_list) < n:
+            continue
+
+        if len(high_list) > n:
+            high_list.pop(0)
+            low_list.pop(0)
+
+        high_n = max(high_list)
+        low_n = min(low_list)
+        rsv = (close - low_n) / (high_n - low_n) * 100
+
+        rsv_list.append(rsv)
+
+        if len(rsv_list) < m1 + m2 - 1:
+            continue
+
+        if len(rsv_list) > m1 + m2 - 1:
+            rsv_list.pop(0)
+
+        k = sma(rsv_list, m1)
+        d = sma(k_list, m2)
+        j = 3 * k - 2 * d
+
+        print(k,d,j)
+        k_list.append(k)
+        d_list.append(d)
+        j_list.append(j)
+
+    return k_list, d_list, j_list
+
+def sma(lst, n):
+    if len(lst) < n:
+        return None
+    return sum(lst[-n:]) / n
 
 def calculate_kdj(data, n=9, m1=3, m2=3):
     """
@@ -25,6 +84,5 @@ def calculate_kdj(data, n=9, m1=3, m2=3):
     j = pd.DataFrame({'j': 3 * k['k'] - 2 * d['d']})  # 计算J值
     kdj_data = pd.concat([k, d, j], axis=1)  # 合并KDJ值
     return kdj_data
-
 
 
--- SHSE.513050/main.py
+++ SHSE.513050/main.py
@@ -1,26 +1,97 @@
 # coding=utf-8
 from __future__ import print_function, absolute_import
 from gm.api import *
+from  kdj  import *
+from datetime import datetime
+
 
 def init(context):
     subscribe(symbols='SHSE.513050', frequency='tick')
     subscribe(symbols='SHSE.513050', frequency='60s')
 
+# def on_tick(context,tick):
+#     # 仓位
+#     position = context.account().position(symbol='SHSE.513050', side=PositionSide_Long)
+#     if position != None and position.available_now:
+#         # 符合预期定量卖出
+#         if tick.price >position.vwap:
+#             order_volume(symbol='SHSE.513050', volume=20000, side=OrderSide_Sell, order_type=OrderType_Limit, position_effect=PositionEffect_Close,price=tick.price)
+#             return
+#         return 
+
+#     # 判断相应的策略 定量买入
+#     order_volume(symbol='SHSE.513050', volume=20000, side=OrderSide_Buy, order_type=OrderType_Limit, position_effect=PositionEffect_Open, price=tick.price)
+
+
+tick_high = []
+tick_low = []
+tick_close = []
+
+high = []
+low = []
+close = []
+min = 0
+max = 0
+kgtdc = 0
+sdfk = 0
+bardk = 0
+
+
 def on_tick(context,tick):
-    # 仓位
+    # 添加逃跑策略，跌了多少或者到达某个时间点
+
+    # symbol	str	标的代码
+    # open	float	日线开盘价
+    # high	float	日线最高价
+    # low	float	日线最低价
+    # price	float	最新价
+    # cum_volume	long	成交总量/最新成交量,累计值（日线成交量）
+    # cum_amount	float	成交总金额/最新成交额,累计值 （日线成交金额）
+    # cum_position	int	合约持仓量(只适用于期货),累计值（股票此值为0）
+    # trade_type	int	交易类型（只适用于期货） 1: ‘双开’, 2: ‘双平’, 3: ‘多开’, 4: ‘空开’, 5: ‘空平’, 6: ‘多平’, 7: ‘多换’, 8: ‘空换’
+    # last_volume	int	瞬时成交量
+    # last_amount	float	瞬时成交额（郑商所last_amount为0）
+    # quotes	[] (list of dict)
+        # bid_p	float	买价
+        # bid_v	int	买量
+        # ask_p	float	卖价
+        # ask_v	int	卖量
+
+    # 考虑把 price 改成买一卖一
+    global min,max,kgtdc,bardk
+    if min == 0 or  tick.price < min:
+        min = tick.price
+    if max == 0 or tick.price > max:
+        max = tick.price
+
+    # 这个是当天的意识吗？数据不对
     position = context.account().position(symbol='SHSE.513050', side=PositionSide_Long)
     if position != None and position.available_now:
-        # 符合预期定量卖出
-        if tick.price >position.vwap:
-            order_volume(symbol='SHSE.513050', volume=20000, side=OrderSide_Sell, order_type=OrderType_Limit, position_effect=PositionEffect_Close,price=tick.price)
-            return
-        return 
+        # 符合预期定量卖出，买一的价格比当前持仓高1毫以上
+        if tick.quotes[0]['bid_p'] > position.vwap+0.001:
+            print(tick.created_at,"卖出",tick.quotes[0]['bid_p']+0.001)
+            order_volume(symbol='SHSE.513050', volume=20000, side=OrderSide_Sell, order_type=OrderType_Limit, position_effect=PositionEffect_Close,price=tick.quotes[0]['bid_p']+0.001)
+        return
+    
 
-    # 判断相应的策略 定量买入
-    order_volume(symbol='SHSE.513050', volume=20000, side=OrderSide_Buy, order_type=OrderType_Limit, position_effect=PositionEffect_Open, price=tick.price)
-
+    if len(high) > 15 :
+        k,d,j = KDJ(pd.Series(high+[max]),pd.Series(low+[min]),pd.Series(close+[tick.price]))
+        # k 和 d 收敛,d 大于 k 慢慢收敛
+        if abs(k.iloc[-1] - d.iloc[-1]) <=0.5:
+            # 两者的差值
+            print(k.iloc[-1] , d.iloc[-1],tick.created_at)
+            kgtdc+=1
+        if  kgtdc > 1 and bardk > 2:
+            # 买进
+            print(tick.created_at,"买进",tick.quotes[0]['ask_p'])
+            order_volume(symbol='SHSE.513050', volume=20000, side=OrderSide_Buy, order_type=OrderType_Limit, position_effect=PositionEffect_Open, price=tick.quotes[0]['ask_p'])
+            kgtdc=0
 
 def on_bar(context,bars):
+    global min,max,bardk,kgtdc
+    min = 0
+    max = 0
+    kgtdc = 0
     # symbol	str	标的代码
     # frequency	str	频率, 支持多种频率, 具体见股票行情数据和期货行情数据
     # open	float	开盘价
@@ -33,27 +104,28 @@ def on_bar(context,bars):
     # bob	datetime.datetime	bar开始时间
     # eob	datetime.datetime	bar结束时间
     # print("bars:",bars)    
+    high.append(bars[0].high)
+    low.append(bars[0].low)
+    close.append(bars[0].close)
+    if len(high) > 15 :
+        k,d,j = KDJ(pd.Series(high),pd.Series(low),pd.Series(close))
+        if d.iloc[-1]>k.iloc[-1]:
+            bardk +=1
+        else:
+            bardk = 0
+        d  = {'t': bars[0].eob.strftime('%Y-%m-%d %H:%M:%S'),'k':k.iloc[-1],'d':d.iloc[-1],'j':j.iloc[-1]}
+        df = pd.DataFrame(d,index=[len(high)])
+        with open('data.csv', mode='a', newline='') as file:
+            df.to_csv(file, header=file.tell()==0, index=False)
     return 
 
 
 if __name__ == '__main__':
-    '''
-        strategy_id策略ID, 由系统生成
-        filename文件名, 请与本文件名保持一致
-        mode运行模式, 实时模式:MODE_LIVE回测模式:MODE_BACKTEST
-        token绑定计算机的ID, 可在系统设置-密钥管理中生成
-        backtest_start_time回测开始时间
-        backtest_end_time回测结束时间
-        backtest_adjust股票复权方式, 不复权:ADJUST_NONE前复权:ADJUST_PREV后复权:ADJUST_POST
-        backtest_initial_cash回测初始资金
-        backtest_commission_ratio回测佣金比例
-        backtest_slippage_ratio回测滑点比例
-        '''
-    run(strategy_id='90ce0128-bbe0-11ed-bb17-5254008182e9',
+    run(strategy_id='ee4a20af-bb3b-11ed-a0a6-d8bbc1dbf93d',
         filename='main.py',
         mode=MODE_BACKTEST,
         token='94ac22c44bf4d986d79e90bf6519f880b4b60411',
-        backtest_start_time='2023-03-06 14:50:00',
+        backtest_start_time='2023-02-06 09:00:00',
         backtest_end_time='2023-03-06 15:00:00',
         backtest_adjust=ADJUST_PREV,
         backtest_initial_cash=100000,
